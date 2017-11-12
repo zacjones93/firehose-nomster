1a577f6f10714d2d4caf49bd0fbfe951e7d33dc8
Zac Jones <zacjones93@gmail.com>
2017-11-12 15:31:28 -0800

    added more validations

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,6 @@
 class Place < ApplicationRecord
   belongs_to :user
 
-  validates :name, presence: true
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :address, :description, presence: true
 end
