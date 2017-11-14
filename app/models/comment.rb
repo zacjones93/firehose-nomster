a23106030334825031b042a21563fdaa3a7e66ae
Zac Jones <zacjones93@gmail.com>
2017-11-13 19:34:37 -0800

    made comment rating a dropdown

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,16 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :place
+
+  RATINGS = {
+    'one star': '1_star',
+    'two star': '2_star',
+    'three star': '3_star',
+    'four star': '4_star',
+    'five star': '5_star',
+  }
+
+  def humanized_rating
+    RATINGS.invert[self.rating]
+  end
 end
